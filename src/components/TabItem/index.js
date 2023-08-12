67b5cf28d36b34004f76a93e403cfe5acea27feb
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-08-12 22:52:24 +0530

    Update index.js

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -1,16 +1,22 @@
 import './index.css'
 
 const TabItem = props => {
-  const {tabDetails, onClickTab} = props
+  const {tabDetails, onClickTab, isActive} = props
   const {tabId, displayText} = tabDetails
 
   const clickTab = () => {
     onClickTab(tabId)
   }
 
+  const activeTab = isActive ? 'active-thumbnail' : ''
+
   return (
     <li className="tab-item">
-      <button onClick={clickTab} className="tab-button" type="button">
+      <button
+        onClick={clickTab}
+        className={`tab-button ${activeTab}`}
+        type="button"
+      >
         {displayText}
       </button>
     </li>
